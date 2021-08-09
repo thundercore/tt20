8a39e6778cfae947f090a22af890cef04f150e51
waynetseng <wayne.tseng@thundercore.tw>
2021-08-09 18:45:57 +0800

    Provide config file for ERC20

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,15 +1,13 @@
 const Token = artifacts.require("./Token");
+const config = require("../config.json");
 
 const web3 = require('web3');
 const BN = web3.utils.BN;
 
-const decimals = new BN(8);
-
-// 1e9 * 10^decimals
-const initialSupply = new BN(0)
-
-const name = 'CXKJ';
-const symbol = 'CXKJ';
+const initialSupply = new BN(config.initialSupply);
+const name = config.name;
+const symbol = config.symbol;
+const decimals = new BN(config.decimals);
 
 module.exports = function(deployer) {
   deployer.deploy(Token, initialSupply, name, symbol, decimals);
